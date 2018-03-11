e228e9e543072d9c13d3d38e065a686e52de2a4e
Tali Greenberg <asgreenb@mail.yu.edu>
2018-03-11 10:25:09 -0400

    Revised app.py

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'uradinsk'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'p2253'
+app.config['MYSQL_DATABASE_DB'] = 'diradinsk'
+app.config['MYSQL_DATABASE_HOST'] = 'mysql://iradinsky:poodleblue96!@ada.sterncs.net:3306'
+
+"""
+app.config['MYSQL_DATABASE_USER'] = 'editor'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'lifehacks'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)"""
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+
+
+@app.route("/")
+def main():
+        return render_template('home.html')
+
+
+@app.route('/post.html')
+def showSignUp():
+        return render_template('post.html')
+
+@app.route('/post.html', methods=['POST'])
+def post():
+    # read the posted values from the UI
+    _hackid = request.form['hack_title']
+    _content = request.form['textarea1']
+    hi_ilana = "hi ilana!!"
+    query = "INSERT INTO TAGS VALUES (hi_ilana)"
+    #query = "INSERT INTO TAGS VALUES ({_hackid})"
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+    return json.dumps({'html':'<span>That\'s a great hack!</span>'})
+
+@app.route('/login page.html')
+def login():
+        _username = request.form['user_name']
+        _password = request.form['password']
+        result = query_db("SELECT U.password FROM Users U WHERE U.email = _username")
+        data = json.dump(result)
+        if data == _password:
+                return render_template('home.html')
+
+@app.route('/signup.html')
+def signup():
+        _username = request.form['user_name']
+        _password = request.form['password']
+        query = "INSERT INTO Users VALUES _username, _password"
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        conn.close()
+                                                                        
+
+if __name__ == "__main__":
+    app.run()
