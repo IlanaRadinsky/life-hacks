bb096e5b9befc57ea9cc13cba93281581429d459
Tali Greenberg <asgreenb@mail.yu.edu>
2018-03-11 00:01:20 -0500

    Revised SQL script

--- lifeHacksTables.sql
+++ lifeHacksTables.sql
@@ -3,14 +3,14 @@ create table Users(
 	email CHAR(40),
     password char(40),
     points INTEGER,
-    PRIMARY KEY (email),
+    PRIMARY KEY (userID),
     UNIQUE KEY (email)
 	);
     
 create table Hacks(
 	hackID INTEGER,
+	userID INTEGER,
 	content CHAR(240),
-    userID INTEGER,
     tag CHAR(40),
     PRIMARY KEY (hackID),
     FOREIGN KEY (userID) REFERENCES Users(userID)
@@ -24,4 +24,16 @@ create table Likes(
     FOREIGN KEY (hackID) references Hacks(hackID)
     );
     
+create table Tags(
+	name CHAR(40),
+    primary key (name)
+    );
+
+create table HacksTags(
+	name CHAR(40),
+    hackID INTEGER,
+    PRIMARY KEY (name, hackID),
+    FOREIGN KEY (name) REFERENCES TagNames(name),
+    FOREIGN KEY (hackID) REFERENCES Hacks(hackID)
+    );
     
