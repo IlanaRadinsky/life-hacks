6c0fd862f0af4a54c2048e71c6e258bdebcc0627
rmoskow1 <rmoskowitz314@gmail.com>
2018-03-11 10:29:58 -0400

    update app.py

--- Website/FlaskApp/app.py
+++ Website/FlaskApp/app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, json, request
 from flask.ext.mysql import MySQL
-
+import sys
 app = Flask(__name__)
 
 mysql = MySQL()
-
+"""
 app.config['MYSQL_DATABASE_USER'] = 'uradinsk'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'p2253'
 app.config['MYSQL_DATABASE_DB'] = 'diradinsk'
-app.config['MYSQL_DATABASE_HOST'] = 'mysql://iradinsky:poodleblue96!@ada.sterncs.net:3306'
+app.config['MYSQL_DATABASE_HOST'] = 'mysql://iradinsky:poodleblue96!@ada.sterncs.net:3306'"""
 
-"""
+
 app.config['MYSQL_DATABASE_USER'] = 'editor'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'lifehacks'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)"""
+mysql.init_app(app)
 
 conn = mysql.connect()
 cursor = conn.cursor()
@@ -37,10 +37,10 @@ def post():
     # read the posted values from the UI
     _hackid = request.form['hack_title']
     _content = request.form['textarea1']
-    hi_ilana = "hi ilana!!"
-    query = "INSERT INTO TAGS VALUES (hi_ilana)"
-    #query = "INSERT INTO TAGS VALUES ({_hackid})"
-    cursor.execute(query)
+    sys.stdout.write(_hackid)
+    #query = "INSERT INTO TAGS VALUES (hi_ilana)"
+    query = "INSERT INTO TAGS VALUES (%s)"
+    cursor.execute(query, (_hackid))
     conn.commit()
     cursor.close()
     conn.close()
